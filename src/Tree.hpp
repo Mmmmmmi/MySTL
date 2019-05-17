f1c1ca0e938bcb9f638cd83cb3abb4a9dec92c7a
Mmmmmmi <812632545@qq.com>
2019-05-17 17:40:46 +0800

    update

--- src/Tree.hpp
+++ src/Tree.hpp
@@ -19,18 +19,60 @@ struct Tree_Node
 template<class T>
 class BinarySearchTree 
 {
+	typedef Tree_Node<T> Node;
+	typedef Tree_Node<T>* PNode;
+	typedef T& reference;
+	typedef BinarySearchTree<T> Self;
+public:
+	BinarySearchTree()
+		:_head(nullptr)
+	{}
 
+	void Destroy()
+	{
+		_Destroy(_head);
+		_head = nullptr;
+	}
+
+	~BinarySearchTree()
+	{
+		Destroy();
+	}
+
+
+private:
+	void _Destroy(PNode root)
+	{
+		if (root != nullptr)
+		{
+			_Destroy(root->_leftChild);
+			_Destroy(root->_rightChild);
+			delete root;
+		}
+	}
+
+	PNode _head;
 };
 
 
 template<class T>
 class AVLTree
 {
+	typedef Tree_Node<T> Node;
+	typedef Tree_Node<T>* PNode;
+public:
 
+private:
+	PNode _head;
 };
 
 template<class T>
 class RedBlackTree
 {
+	typedef Tree_Node<T> Node;
+	typedef Tree_Node<T>* PNode;
+public:
 
+private:
+	PNode _head;
 };
