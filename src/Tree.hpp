f96935df1d511eed85f6f311a0d6982ed08d2220
Mmmmmmi <812632545@qq.com>
2019-05-18 23:52:57 +0800

    update

--- src/Tree.hpp
+++ src/Tree.hpp
@@ -1,77 +1,93 @@
 #pragma once
 
-
 template<class T>
 struct Tree_Node
 {
-	typedef Tree_Node<T> Node;
-	typedef Tree_Node<T>* PNode;
+    typedef Tree_Node<T> Node;
+    typedef Tree_Node<T>* PNode;
 
-	T _data;
-	PNode _leftChild;
-	PNode _rightChild;
-	PNode _patent;
+    T _data;
+    PNode _leftChild;
+    PNode _rightChild;
+    PNode _patent;
 };
-
 
 
 template<class T>
 class BinarySearchTree 
 {
-	typedef Tree_Node<T> Node;
-	typedef Tree_Node<T>* PNode;
-	typedef T& reference;
-	typedef BinarySearchTree<T> Self;
+    typedef Tree_Node<T> Node;
+    typedef Tree_Node<T>* PNode;
+    typedef T& reference;
+    typedef BinarySearchTree<T> Self;
 public:
-	BinarySearchTree()
-		:_head(nullptr)
-	{}
+    BinarySearchTree()
+        :_head(nullptr)
+    {}
 
-	void Destroy()
-	{
-		_Destroy(_head);
-		_head = nullptr;
-	}
+    void Destroy()
+    {
+        _Destroy(_head);
+        _head = nullptr;
+    }
 
-	~BinarySearchTree()
-	{
-		Destroy();
-	}
+    ~BinarySearchTree()
+    {
+        Destroy();
+    }
+
+    bool Insert(const T& data)
+    {
+        
+        return true; 
+    }
+
+    bool Erase(const T& data)
+    {
+
+    }
+
+    PNode Find(const T& data)
+    {
+
+    }
+
+
 
 
 private:
-	void _Destroy(PNode root)
-	{
-		if (root != nullptr)
-		{
-			_Destroy(root->_leftChild);
-			_Destroy(root->_rightChild);
-			delete root;
-		}
-	}
+    void _Destroy(PNode root)
+    {
+        if (root != nullptr)
+        {
+            _Destroy(root->_leftChild);
+            _Destroy(root->_rightChild);
+            delete root;
 
-	PNode _head;
+        }
+    }
+
+    PNode _head;
 };
-
 
 template<class T>
 class AVLTree
 {
-	typedef Tree_Node<T> Node;
-	typedef Tree_Node<T>* PNode;
+    typedef Tree_Node<T> Node;
+    typedef Tree_Node<T>* PNode;
 public:
 
 private:
-	PNode _head;
+    PNode _head;
 };
 
 template<class T>
 class RedBlackTree
 {
-	typedef Tree_Node<T> Node;
-	typedef Tree_Node<T>* PNode;
+    typedef Tree_Node<T> Node;
+    typedef Tree_Node<T>* PNode;
 public:
 
 private:
-	PNode _head;
+    PNode _head;
 };
